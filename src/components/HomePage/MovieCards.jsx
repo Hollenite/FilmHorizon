7bc5cbf8d5cfc00467d71d137ef36145e2d71e43
Hollenite <ariyanpradhan112@gmail.com>
2026-02-11 18:51:32 +0530

    update the project

---
+++ src/components/HomePage/MovieCards.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./MovieCards.css";
+
+function MovieCards({ movies }) {
+  return (
+    <>
+      <div className="movie-grid">
+        {movies.map((movie)=> (
+            <div className="movie-cards" key={movie.id}> 
+                <img className="movie-image" src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}/>
+                <div className="info">
+                    <h3 className="movie-title">{movie.title}</h3>   
+                </div>
+            </div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default MovieCards;
--- src/components/HomePage/MovieSection.jsx
+++ src/components/HomePage/MovieSection.jsx
@@ -1,13 +1,16 @@
 import { useState, useEffect } from "react";
+import MovieCards from "./MovieCards";
 
 function MoviesSection() {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
     async function getMovies() {
-      const data = await fetch(`URL`);
+      const url = `https://api.themoviedb.org/3/movie/popular?api_key=${import.meta.env.VITE_TMDB_API_KEY}`;
+      const data = await fetch(url);
       const json = await data.json();
       setMovies(json.results);
+      console.log(json.results);
     }
     getMovies();
   }, []);
@@ -15,7 +18,7 @@ function MoviesSection() {
   return (
     <section className="movies-section">
       <h2>Popular Movies</h2>
-      {/* your movie grid here later */}
+      {<MovieCards movies={movies} />}
     </section>
   );
 }
