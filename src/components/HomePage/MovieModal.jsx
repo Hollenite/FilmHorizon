d0dd68def841795e4da17e1520feae143d7195b3
Hollenite <ariyanpradhan112@gmail.com>
2026-02-11 22:57:37 +0530

    update UI

--- src/components/HomePage/MovieModal.jsx
+++ src/components/HomePage/MovieModal.jsx
@@ -1,9 +1,28 @@
+import "./MovieModal.css";
+
 function MovieModal({ movie, onClose }) {
-    return (
-        <>
-            <p> movie clicked </p>
-        </>
-    )
+  return (
+    <>
+      <div className="modal-overlay">
+        <div className="modal-content">
+          <img
+            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+            alt={movie.title}
+          />
+          <div className="movie-details">
+            <p>{movie.title}</p>
+            <p>{movie.overview}</p>
+            <p>{movie.release_date}</p>
+            <p>{movie.vote_average}</p>
+            <p>{movie.overview}</p>
+          </div>
+          <button className="close-button" onClick={onClose}>
+            Close
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default MovieModal;
